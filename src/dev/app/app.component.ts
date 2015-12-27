f41322277ce0517ab8ff46ca2fd290cace5bbd89
Tiberiu Popescu <tiberiu.popescu@bedegaming.com>
2015-12-27 21:22:12 +0000

    add routes

--- src/dev/app/app.component.ts
+++ src/dev/app/app.component.ts
@@ -1,7 +1,12 @@
 ﻿import {Component}         from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
 import {HeroFormComponent} from './form/hero-form.component';
 import {BirthdayOne} from './birthday/birthday-one.component';
 import {PowerBoostCalculator} from './power/boost-calculator.component';
+
+import {CrisisListComponent}   from './crisis-list.component';
+import {HeroListComponent}     from './hero-list.component';
 
 @Component({
     selector: 'my-app',
@@ -9,8 +14,18 @@ import {PowerBoostCalculator} from './power/boost-calculator.component';
                <hero-birthday></hero-birthday>
                <power-boost-calculator></power-boost-calculator>
                <hero-message></hero-message>
-              `,
-    directives: [HeroFormComponent, BirthdayOne, PowerBoostCalculator]
+
+              <h1>Component Router</h1>
+              <a [routerLink]="['CrisisCenter']">Crisis Center</a>
+              <a [routerLink]="['Heroes']">Heroes</a>
+              <router-outlet></router-outlet>
+  `,
+    directives: [HeroFormComponent, BirthdayOne, PowerBoostCalculator, ROUTER_DIRECTIVES]
 })
+
+@RouteConfig([
+    { path: '/crisis-center', name: 'CrisisCenter', component: CrisisListComponent },
+    { path: '/heroes', name: 'Heroes', component: HeroListComponent }
+])
 
 export class AppComponent { }
