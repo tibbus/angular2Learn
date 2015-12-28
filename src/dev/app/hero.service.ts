8013d80782bfc9be778e402141be914f33a97bdb
Tiberiu Popescu <Tiberiu Popescu>
2015-12-28 01:27:21 +0000

    remove old comp, add service, dependency injection

---
+++ src/dev/app/hero.service.ts
@@ -0,0 +1,17 @@
+import {HEROES} from './mock-heroes';
+import {Injectable} from 'angular2/core';
+import {Hero} from './hero';
+
+@Injectable()
+
+export class HeroService {
+    getHeroes() {
+        return Promise.resolve(HEROES);
+    }
+
+    getHeroesSlowly() {
+        return new Promise(resolve =>
+            setTimeout(() => resolve(HEROES), 2000) // 2 seconds
+        );
+    }
+}
